50712f7f14e904c8c9f7157dec907912a3f33e67
Jean M. Lescure <jeanmlescure@gmail.com>
2020-07-12 17:25:31 -0600

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var http = _interopDefault(require('http'));
+
+var version = '0.0.1';
+
+var Greeter = /** @class */ (function () {
+    function Greeter(greeting) {
+        this.greeting = greeting;
+    }
+    Greeter.prototype.greet = function () {
+        var _a = this.greeting, greeting = _a.greeting, greetingTarget = _a.greetingTarget, version = _a.version;
+        return "Greeter version " + version + " says: " + greeting + " " + greetingTarget + "!";
+    };
+    return Greeter;
+}());
+
+var legacyStatus = {
+  status: 200,
+};
+var legacyStatus_1 = legacyStatus.status;
+
+var _a = process.env, HOSTNAME = _a.HOSTNAME, PORT = _a.PORT, GREETING_TARGET = _a.GREETING_TARGET;
+var greeter = new Greeter({
+    greeting: 'Hello',
+    greetingTarget: GREETING_TARGET,
+    version: version,
+});
+var server = http.createServer(function (req, res) {
+    res.statusCode = legacyStatus_1;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(greeter.greet());
+});
+server.listen(parseInt(PORT, 10), HOSTNAME, function () {
+    console.log("Server running at http://" + HOSTNAME + ":" + PORT + "/");
+});
+//# sourceMappingURL=index.js.map
---
+++ rollup.config.ts
@@ -0,0 +1,43 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import run from '@rollup/plugin-run';
+
+const pkg = require('./package.json');
+
+const dev = process.env.ROLLUP_WATCH === 'true';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+      }),
+      commonjs({
+        sourceMap: true,
+      }),
+      resolve({
+        preferBuiltins: true,
+      }),
+      dev && run({
+        execArgv: ['-r', 'dotenv/config', '-r', 'source-map-support/register', '--inspect'],
+      }),
+    ],
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: [
+      ...Object.keys(pkg.dependencies),
+      ...Object.keys(pkg.devDependencies),
+    ],
+    watch: {
+      include: 'src/**',
+    },
+  }
+];
