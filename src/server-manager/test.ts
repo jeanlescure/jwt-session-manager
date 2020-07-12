f9e09b3b6c80c7b6d7cc869069a10d0c3498d728
Jean M. Lescure <jeanmlescure@gmail.com>
2020-07-12 17:40:28 -0600

    implement version export

---
+++ src/server-manager/test.ts
@@ -0,0 +1,1 @@
+import ServerJWTSessionManager from './';
---
+++ src/test.ts
@@ -0,0 +1,6 @@
+import {version} from './';
+
+test('version is exported', () => {
+  expect(typeof version).toBe('string');
+  expect((/^\d+\.\d+/).test(version));
+});
