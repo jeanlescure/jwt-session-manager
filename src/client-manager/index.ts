faced1781138dee9cbcf738a5ba757ba47c7c81c
Jean M. Lescure <jeanmlescure@gmail.com>
2020-07-12 18:28:24 -0600

    add method to get request token to client manager

--- src/client-manager/index.ts
+++ src/client-manager/index.ts
@@ -1,3 +1,25 @@
+import {ClientInitOptions} from './interfaces';
+
 export default class ClientJWTSessionManager {
-  
+  sessionRequestToken: string;
+  clientOptions: ClientInitOptions = {
+    getSessionRequestTokenHandler: async () => {},
+  };
+
+  constructor(options: ClientInitOptions) {
+    this.clientOptions = {
+      ...this.clientOptions,
+      ...options,
+    };
+  }
+
+  async getSessionRequestToken() {
+    const {
+      getSessionRequestTokenHandler,
+    } = this.clientOptions;
+
+    const requestTokenResponse = await getSessionRequestTokenHandler();
+
+    this.sessionRequestToken = requestTokenResponse;
+  }
 };
--- src/client-manager/test.ts
+++ src/client-manager/test.ts
@@ -1,1 +1,21 @@
+import * as jwt from 'jsonwebtoken';
 import ClientJWTSessionManager from './';
+
+const mockRequestToken = jwt.sign({
+  exp: Math.floor(Date.now() / 1000) + (60 * 60),
+  data: 'foobar'
+}, 'secret');
+
+test('can ask for session request token', async() => {
+  const clientSessionManager = new ClientJWTSessionManager({
+    getSessionRequestTokenHandler: async () => {
+      return new Promise((resolve, reject) => resolve(mockRequestToken));
+    },
+  });
+
+  expect(clientSessionManager.sessionRequestToken).toBeUndefined();
+
+  await clientSessionManager.getSessionRequestToken();
+
+  jwt.verify(clientSessionManager.sessionRequestToken, 'secret');
+});
