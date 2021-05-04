8be5b521ec4e6db55b4f5bfe826b046928428c91
Jean M. Lescure <jeanmlescure@gmail.com>
2021-05-04 14:37:27 -0600

    chore: get release ready

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ var path = _interopDefault(require('path'));
 var ShortUniqueId = _interopDefault(require('short-unique-id'));
 var jwt = require('jsonwebtoken');
 
-var version = '1.1.2';
+var version = '1.1.3';
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
